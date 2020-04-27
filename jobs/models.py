2cb2f76d17a2f6d68fd62eaa13cb93104ce27bca
Fabián <fabian.gutierrez.gomez@gmail.com>
2020-04-27 04:25:56 -0500

    Download Pillow, modify models.py, and a migrate doing

--- jobs/models.py
+++ jobs/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+# Classn job, en 2: image, que son imágenes, y summary, que es lo que 
+#irá escrito
+class Job(models.Model):
+	image=models.ImageField(upload_to='images/')
+	summary=models.CharField(max_length=200)
+ 
