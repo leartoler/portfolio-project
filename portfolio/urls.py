11be7855f1369b92346df61921a46120979d07ed
Fabián <fabian.gutierrez.gomez@gmail.com>
2020-04-28 06:14:11 -0500

    show jobs and a better understandig of bootstrap

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -17,11 +17,15 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+import jobs.views
+
+
 
 #Se le agrega el path, y posteriormente:
 #Static: el path, el camino a seguir, el media url
 #Media, el media_root, o la información que debe de contener
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', jobs.views.home, name='home'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
